0a49de100f9bf62a477c61203055333eb72de931
MOnicah <monicawughanga@gmail.com>
2022-06-06 17:25:45 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+document.getElementById('btn').addEventListener('click', ()=> {
+    const weight = document.getElementById('weight').value;
+    const unit = document.getElementById('select').value;
+    // console.log(weight);
+
+    // converting
+    if (unit == 'Kilograms'){
+        if (weight == 0) or (weight == '');{
+            const conversion = (weight * 2.2);
+
+        }
+        
+        
+
+    }
+        
+})
+
+
+
